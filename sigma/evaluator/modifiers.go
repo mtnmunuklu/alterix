e319a1b87c1755acf36632a741a455b8ff7ee524
metinmunuklu <mtnmunuklu@gmail.com>
2023-06-02 16:40:20 +0300

    Deleted tolower for values and added string check

--- sigma/evaluator/modifiers.go
+++ sigma/evaluator/modifiers.go
@@ -13,10 +13,10 @@ func baseComparator(field string, matcherValue string) string {
 	switch {
 	case matcherValue == "null":
 		// The "null" value is treated as a special case and is used to match when a field is not present in the event
-		return fmt.Sprintf("%v = ''", strings.ToLower(field))
+		return fmt.Sprintf("%v = ''", coerceString(field))
 	default:
 		// Perform a case-insensitive string comparison
-		return fmt.Sprintf("%v = '%v'", strings.ToLower(field), strings.ToLower(matcherValue))
+		return fmt.Sprintf("%v = '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 	}
 }
 
@@ -28,61 +28,72 @@ var modifiers = map[string]valueModifier{
 	// The "contains" modifier matches when the field contains the specified value (case-insensitive)
 	"contains": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v like '%%%v%%'", strings.ToLower(field), strings.ToLower(matcherValue))
+			return fmt.Sprintf("%v like '%%%v%%'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "endswith" modifier matches when the field ends with the specified value (case-insensitive)
 	"endswith": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v like '%%%v'", strings.ToLower(field), strings.ToLower(matcherValue))
+			return fmt.Sprintf("%v like '%%%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "startswith" modifier matches when the field starts with the specified value (case-insensitive)
 	"startswith": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v like '%v%%'", strings.ToLower(field), strings.ToLower(matcherValue))
+			return fmt.Sprintf("%v like '%v%%'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "base64" modifier matches when the field matches the specified value encoded in Base64
 	"base64": func(next valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v base64 '%v'", strings.ToLower(field), matcherValue)
+			return fmt.Sprintf("%v base64 '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "re" modifier matches when the field matches the specified regular expression
 	"re": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v re '%v'", strings.ToLower(field), matcherValue)
+			return fmt.Sprintf("%v re '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "cidr" modifier matches when the field is an IP address that matches the specified CIDR range
 	"cidr": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v cidr '%v'", strings.ToLower(field), matcherValue)
+			return fmt.Sprintf("%v cidr '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "gt" modifier matches when the field is greater than the specified value (case-insensitive)
 	"gt": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v > '%v'", strings.ToLower(field), strings.ToLower(matcherValue))
+			return fmt.Sprintf("%v > '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "gte" modifier matches when the field is greater than or equal to the specified value (case-insensitive)
 	"gte": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v >= '%v'", strings.ToLower(field), strings.ToLower(matcherValue))
+			return fmt.Sprintf("%v >= '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "lt" modifier matches when the field is less than the specified value (case-insensitive)
 	"lt": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v < '%v'", strings.ToLower(field), strings.ToLower(matcherValue))
+			return fmt.Sprintf("%v < '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
 	// The "lte" modifier matches when the field is less than or equal to the specified value (case-insensitive)
 	"lte": func(_ valueComparator) valueComparator {
 		return func(field string, matcherValue string) string {
-			return fmt.Sprintf("%v <= '%v'", strings.ToLower(field), strings.ToLower(matcherValue))
+			return fmt.Sprintf("%v <= '%v'", strings.ToLower(coerceString(field)), coerceString(matcherValue))
 		}
 	},
+}
+
+func coerceString(v interface{}) string {
+	switch vv := v.(type) {
+	case string:
+		return vv
+	case []byte:
+		return string(vv)
+	default:
+		return fmt.Sprint(vv)
+	}
 }
